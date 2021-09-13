2684571283c46a6b49b1165ef6352d9a5f7147ed
Amano <e20008@e.hiroshima-cu.ac.jp>
2021-09-13 19:16:40 +0900

    OCR機能の削除

--- main.js
+++ main.js
@@ -3,31 +3,8 @@ new Vue({
     vuetify: new Vuetify(),
     data(){
         return{
-            img: [''],
-            renderFlag: false
         };
     },
     methods:{
-        registerFiles(files){
-            console.log("meow");
-            if(files){
-                // filesは配列？
-                console.log("meow-file-get");
-
-                this.renderFlag = false
-                this.img.splice(0);
-                // ループ処理でやらなきゃ？
-                for(let i = 0; i < files.length; i++){
-                    const reader = new FileReader();
-                    reader.onload = f => {
-                        this.img.push(f.target.result);
-                    };
-                    reader.readAsDataURL(files[i]);
-                }
-                this.renderFlag = true;
-                console.log("meow-file-read");
-                console.log(this.img);
-            }
-        }
     }
 })
