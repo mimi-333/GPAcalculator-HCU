8a20fcf878449feafebec1d70e4a20044483203c
Amano <e20008@e.hiroshima-cu.ac.jp>
2021-09-15 04:00:29 +0900

    だいたい完成(表示機能、および表示に用いるデータ生成機能の追加)

--- main.js
+++ main.js
@@ -4,52 +4,84 @@ const Semester = function(){
     this.優 = 0;
     this.良 = 0;
     this.可 = 0;
+    this.calcScore = function(){
+        return this.秀 * 4 + this.優 * 3 + this.良 * 2 + this.可;
+    };
+    this.calcGPA = function(){
+        if(this.sum == 0){
+            return 0;
+        }
+        return Math.round(this.calcScore() * 100 / this.sum) / 100;
+    };
+    this.calcGain = function(){
+        return this.秀 + this.優 + this.良 + this.可;
+    };
+    this.calcLoss = function(){
+        return this.sum - this.calcGain();
+    };
 };
 
 new Vue({
     el: '#app',
     vuetify: new Vuetify(),
-    computed:{
-        search_pattern: () => {
-            return new RegExp('(前期|後期).+(\\d+\.\\d+).+年生\\s*(不?可\\*?|良|優|秀)', 'g')
-        },
-        items: () => {return [1,2,3]}
-    },
     data(){
         return{
-            target_text: ""
+            target_text: "",
+            semesters: []
         };
+    },
+    computed:{
+        search_pattern: function(){
+            return new RegExp('(前期|後期).+(\\d+\.\\d+).+年生\\s*(不?可\\*?|良|優|秀)', 'g')
+        },
+        formatted_semesters: function(){
+            if(this.semesters.length != 0){
+                const semesters_sum = new Semester();
+                semesters_sum.sum = this.semesters[0].sum + this.semesters[1].sum;
+                semesters_sum.秀 = this.semesters[0].秀 + this.semesters[1].秀;
+                semesters_sum.優 = this.semesters[0].優 + this.semesters[1].優;
+                semesters_sum.良 = this.semesters[0].良 + this.semesters[1].良;
+                semesters_sum.可 = this.semesters[0].可 + this.semesters[1].可;
+
+                return [["前期", this.semesters[0]],
+                        ["後期", this.semesters[1]],
+                        ["通期", semesters_sum]];
+            }else{
+                return [];
+            }
+        }
     },
     methods:{
         analyze(){
             const matches = this.target_text.matchAll(this.search_pattern);
 
-            const sem = [new Semester(), new Semester()];
+            this.semesters = [new Semester(), new Semester()];
+
             for (const match of matches) {
                 let idx = 0;
                 if(match[1] !== "前期"){
                     idx = 1;
                 }
 
-                sem[idx].sum += parseFloat(match[2]);
+                const num = parseFloat(match[2]);
+                this.semesters[idx].sum += num;
                 switch(match[3]){
                 case "秀":
-                    sem[idx].秀 += parseFloat(match[2]);
+                    this.semesters[idx].秀 += num;
                     break;
                 case "優":
-                    sem[idx].優 += parseFloat(match[2]);
+                    this.semesters[idx].優 += num;
                     break;
                 case "良":
-                    sem[idx].良 += parseFloat(match[2]);
+                    this.semesters[idx].良 += num;
                     break;
                 case "可":
-                    sem[idx].可 += parseFloat(match[2]);
+                    this.semesters[idx].可 += num;
                     break;
                 default:
                     break;
                 }
             }
-            console.log(sem);
         }
     }
 })
